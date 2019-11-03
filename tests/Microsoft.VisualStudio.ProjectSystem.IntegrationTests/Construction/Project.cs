092ccb1e04ea2dd72129eb5a3ff56ce8c089c478
Drew Noakes <git@drewnoakes.com>
2019-11-04 09:17:14 +1100

    Support testing with legacy projects too

--- tests/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/Construction/Project.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/Construction/Project.cs
@@ -21,6 +21,94 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
         void Save(string solutionRoot);
     }
 
+    public enum OutputType
+    {
+        Library,
+        Exe
+    }
+
+    public sealed class LegacyProject : IProject
+    {
+        private static readonly Guid s_legacyProjectTypeGuid = Guid.Parse("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC");
+
+        public string ProjectName { get; } = "LegacyProject_" + Guid.NewGuid().ToString("N").Substring(0, 12);
+        public string ProjectFileName => $"{ProjectName}.csproj";
+        public string RelativeProjectFilePath => $"{ProjectName}\\{ProjectName}.csproj";
+        public Guid ProjectGuid { get; } = Guid.NewGuid();
+        public object ProjectTypeGuid => s_legacyProjectTypeGuid;
+
+        public ProjectTestExtension? Extension { get; set; }
+
+        private readonly OutputType _outputType;
+        private readonly string _targetFrameworkMoniker;
+
+        public LegacyProject(OutputType outputType, string targetFrameworkMoniker)
+        {
+            _outputType = outputType;
+            _targetFrameworkMoniker = targetFrameworkMoniker;
+        }
+
+        public void Save(string solutionRoot)
+        {
+            var xml = $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project ToolsVersion=""15.0"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <Import Project=""$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props"" Condition=""Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')"" />
+  <PropertyGroup>
+    <Configuration Condition="" '$(Configuration)' == '' "">Debug</Configuration>
+    <Platform Condition="" '$(Platform)' == '' "">AnyCPU</Platform>
+    <ProjectGuid>{ProjectGuid:D}</ProjectGuid>
+    <OutputType>{_outputType}</OutputType>
+    <AppDesignerFolder>Properties</AppDesignerFolder>
+    <RootNamespace>{ProjectName}</RootNamespace>
+    <AssemblyName>{ProjectName}</AssemblyName>
+    <TargetFrameworkVersion>{_targetFrameworkMoniker}</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+    <Deterministic>true</Deterministic>
+  </PropertyGroup>
+  <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' "">
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' "">
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include=""System""/>
+    <Reference Include=""System.Core""/>
+    <Reference Include=""System.Xml.Linq""/>
+    <Reference Include=""System.Data.DataSetExtensions""/>
+    <Reference Include=""Microsoft.CSharp""/>
+    <Reference Include=""System.Data""/>
+    <Reference Include=""System.Net.Http""/>
+    <Reference Include=""System.Xml""/>
+  </ItemGroup>
+<!--
+  <ItemGroup>
+    <Compile Include=""Class1.cs"" />
+    <Compile Include=""Properties\AssemblyInfo.cs"" />
+  </ItemGroup>
+-->
+  <Import Project=""$(MSBuildToolsPath)\Microsoft.CSharp.targets"" />
+ </Project>
+";
+            var projectRoot = Path.Combine(solutionRoot, ProjectName);
+
+            Directory.CreateDirectory(projectRoot);
+
+            File.WriteAllText(Path.Combine(solutionRoot, RelativeProjectFilePath), xml);
+        }
+    }
+
     /// <summary>
     /// Defines a <c>.csproj</c> file to be created when using <see cref="ProjectLayoutTestBase"/>.
     /// </summary>
